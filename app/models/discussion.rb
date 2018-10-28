c85cfba7e9cbcec0fde9b6ef3d67be90f94532eb
josephhyatt <josephnhyatt@gmail.com>
2018-10-28 13:59:59 -0700

    fixed spelling error 'replies' that was causing error in views/discussions/show.html.erb

--- app/models/discussion.rb
+++ app/models/discussion.rb
@@ -3,7 +3,7 @@ class Discussion < ApplicationRecord
   # discussion belongs_to a channel and a user in db/schema.rb
   belongs_to :channel 
   belongs_to :user 
-  has_many :replys, dependent: :destroy 
+  has_many :replies, dependent: :destroy 
   # validates hooks into the title and content parameter of a discussion
   # wont allow user to submit unless there is content within them
   validates :title, :content, presence: true
